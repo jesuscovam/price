a60fe1ad17b9ebcec7f17b699b711ba81143bb2d
jesuscovam <46403187+jesuscovam@users.noreply.github.com>
2019-02-02 08:06:38 -0500

    Update price.py

--- price.py
+++ price.py
@@ -1,4 +1,5 @@
+import pyperclip
 def p(amount, nights):
-    # 19 is the current exchange rate of the hotel
-    return amount * 19 / nights
-
+    result = amount * 19 / nights
+    pyperclip.copy(result)
+    return result
